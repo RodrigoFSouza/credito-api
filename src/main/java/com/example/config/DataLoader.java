7b86b7ddab087b6157e1e4a6e6835874a009c605
Rodrigo Souza <rodrigo.souza@goharry.com.br>
2025-07-07 20:40:08 -0300

    feat: adicionando valores default para testes

--- src/main/java/com/example/config/DataLoader.java
+++ src/main/java/com/example/config/DataLoader.java
@@ -1,70 +1,71 @@
 package com.example.config;
 
+import com.example.domain.entities.Credito;
 import com.example.repositories.CreditoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Component
+@RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
     
-    @Autowired
-    private CreditoRepository creditoRepository;
+    private final CreditoRepository creditoRepository;
     
     @Override
     public void run(String... args) throws Exception {
-        // Verifica se já existem dados na tabela
         if (creditoRepository.count() == 0) {
             carregarDadosIniciais();
         }
     }
     
     private void carregarDadosIniciais() {
-        // Dados de exemplo conforme o script fornecido
-        /*
-        Credito credito1 = new Credito(
-            "123456",
-            "7891011",
-            LocalDate.of(2024, 2, 25),
-            new BigDecimal("1500.75"),
-            "ISSQN",
-            true,
-            new BigDecimal("5.0"),
-            new BigDecimal("30000.00"),
-            new BigDecimal("5000.00"),
-            new BigDecimal("25000.00")
-        );
-        
-        Credito credito2 = new Credito(
-            "789012",
-            "7891011",
-            LocalDate.of(2024, 2, 26),
-            new BigDecimal("1200.50"),
-            "ISSQN",
-            false,
-            new BigDecimal("4.5"),
-            new BigDecimal("25000.00"),
-            new BigDecimal("4000.00"),
-            new BigDecimal("21000.00")
-        );
-        
-        Credito credito3 = new Credito(
-            "654321",
-            "1122334",
-            LocalDate.of(2024, 1, 15),
-            new BigDecimal("800.50"),
-            "Outros",
-            true,
-            new BigDecimal("3.5"),
-            new BigDecimal("20000.00"),
-            new BigDecimal("3000.00"),
-            new BigDecimal("17000.00")
-        );
+        Credito credito1 = Credito.builder()
+                .numeroCredito("123456")
+                .numeroNfse("7891011")
+                .dataConstituicao(LocalDate.of(2024, 2, 25))
+                .valorIssqn(new BigDecimal("1500.75"))
+                .tipoCredito("ISSQN")
+                .simplesNacional(true)
+                .aliquota(new BigDecimal("5.0"))
+                .valorFaturado(new BigDecimal("30000.00"))
+                .valorDeducao(new BigDecimal("5000.00"))
+                .baseCalculo(new BigDecimal("25000.00"))
+                .build();
+
+        Credito credito2 = Credito.builder()
+                .numeroCredito("789012")
+                .numeroNfse("7891011")
+                .dataConstituicao(LocalDate.of(2024, 2, 26))
+                .valorIssqn(new BigDecimal("1200.50"))
+                .tipoCredito("ISSQN")
+                .simplesNacional(false)
+                .aliquota(new BigDecimal("4.5"))
+                .valorFaturado(new BigDecimal("25000.00"))
+                .valorDeducao(new BigDecimal("4000.00"))
+                .baseCalculo(new BigDecimal("21000.00"))
+                .build();
+
+        Credito credito3 = Credito.builder()
+                .numeroCredito("654321")
+                .numeroNfse("1122334")
+                .dataConstituicao(LocalDate.of(2024, 1, 15))
+                .valorIssqn(new BigDecimal("800.50"))
+                .tipoCredito("Outros")
+                .simplesNacional(true)
+                .aliquota(new BigDecimal("3.5"))
+                .valorFaturado(new BigDecimal("20000.00"))
+                .valorDeducao(new BigDecimal("3000.00"))
+                .baseCalculo(new BigDecimal("17000.00"))
+                .build();
         
         creditoRepository.save(credito1);
         creditoRepository.save(credito2);
         creditoRepository.save(credito3);
-        */
+
         System.out.println("Dados iniciais carregados com sucesso!");
     }
 }
